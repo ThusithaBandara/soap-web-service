87407e2720343b76ab7683d95f6b8b62adad4970
ThusithaBandara <thusitharmtgk95@gmail.com>
2019-10-09 17:16:46 +0530

    need jaxb

---
+++ src/main/java/com/example/soap/webservices/soapcourcemanagement/SoapCourceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.soap.webservices.soapcourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soap/webservices/soapcourcemanagement/SoapCourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soap.webservices.soapcourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
